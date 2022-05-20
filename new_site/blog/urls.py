ef3158fa3b4c4ccd811d4438d5ed87ff65110303
Sanqn <sandisvert@gmail.com>
2022-05-20 20:28:50 +0300

    create page detail

--- new_site/blog/urls.py
+++ new_site/blog/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from . import views
-from .views import MainHome
+from .views import MainHome, PostDetailView
 
 
 urlpatterns = [
     path('', MainHome.as_view(), name='home'),
+    path('home/<slug>', PostDetailView.as_view(), name='detail_page'),
 ]
 
 
--- new_site/blog/views.py
+++ new_site/blog/views.py
@@ -1,8 +1,9 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import HttpResponse, Http404
+from django.shortcuts import render, get_object_or_404
 from django.views import View
 from .models import Post
 from django.core.paginator import Paginator
+from django.core.exceptions import ObjectDoesNotExist
 
 
 ## Post without pagination
@@ -19,3 +20,9 @@ class MainHome(View):
         page_number = request.GET.get('page')
         page_obj = paginator.get_page(page_number)
         return render(request, 'blog/index.html', context={'page_obj': page_obj})
+
+
+class PostDetailView(View):
+    def get(self, request, slug, *args, **kwargs):
+        post = get_object_or_404(Post, url=slug)
+        return render(request, 'blog/post_detail.html', context={'post': post})
